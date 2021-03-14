c198cc476bab55cd7ebf13b70554851c037cabbc
juani <juanisantoro@gmail.com>
2021-03-14 02:30:44 -0300

    Set tipography in home

--- Api/BirrasApp/ClientApp/src/components/Home.js
+++ Api/BirrasApp/ClientApp/src/components/Home.js
@@ -1,4 +1,5 @@
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import Lottie from "lottie-react";
 import React from "react";
 import animationData from "../lotties/19713-six-pack-beer.json";
@@ -25,13 +26,17 @@ export default () => {
   return (
     <Grid container alignContent='center'>
       <Grid item xs={12}>
-        <h1>Santander Tecnología</h1>
-        <p>Bienvenido a santander meetups</p>
-        <ul>
+        <Typography variant='h4' color='textSecondary' gutterBottom>
+          Santander Tecnología
+        </Typography>
+        <Typography variant='h5' color='textSecondary' gutterBottom>
+          Bienvenido a santander meetups
+        </Typography>
+        <Typography variant={"caption"} color='textSecondary' gutterBottom>
           En Santander Tecnología queremos armar las mejores meetups y para eso
           planeamos hacer una App que nos ayude a lograr que no falte lo
           fundamental... ¡Birras!
-        </ul>
+        </Typography>
       </Grid>
       <Grid item xs={4} md={4}></Grid>
       <Grid item xs={8} md={8}>
