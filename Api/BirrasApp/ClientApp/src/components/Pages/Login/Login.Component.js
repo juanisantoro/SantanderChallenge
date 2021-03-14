ed01bbf71c810e0c3b7edfd162b314df51e0e20a
juani <juanisantoro@gmail.com>
2021-03-14 03:29:51 -0300

    Fix login component

--- Api/BirrasApp/ClientApp/src/components/Pages/Login/Login.Component.js
+++ Api/BirrasApp/ClientApp/src/components/Pages/Login/Login.Component.js
@@ -1,5 +1,7 @@
 import { Button, CircularProgress } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Link from "@material-ui/core/Link";
@@ -12,7 +14,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "../../../App";
 import logo from "../../../logosSantander/elefanteSantander.gif";
 import wheatherService from "../../../services/weatherService";
-
 const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
 function Copyright() {
   return (
@@ -76,62 +77,66 @@ export default function SignIn() {
   };
 
   return (
-    <Container component='main' maxWidth='xs'>
-      <CssBaseline />
-      <div id='login'></div>
-      <div className={classes.paper}>
-        <img src={logo} alt='loading...' />
+    <Card variant='outlined'>
+      <CardContent>
+        <Container component='main' maxWidth='xs'>
+          <CssBaseline />
+          <div id='login'></div>
+          <div className={classes.paper}>
+            <img src={logo} alt='loading...' />
 
-        {/* <Typography component='h1' variant='h5'>
+            {/* <Typography component='h1' variant='h5'>
           Ingresar
         </Typography> */}
-        <form className={classes.form} noValidate>
-          <TextField
-            placeholder='user o admin'
-            onChange={setearUser}
-            variant='outlined'
-            margin='normal'
-            required
-            error={user === "" ? true : false}
-            fullWidth
-            id='email'
-            label='Usuario'
-            name='email'
-            autoComplete='email'
-            autoFocus
-            value={user}
-          />
-          <TextField
-            placeholder='123456'
-            onChange={setearPass}
-            variant='outlined'
-            margin='normal'
-            error={pass === "" ? true : false}
-            required
-            fullWidth
-            name='password'
-            label='Contraseña'
-            type='password'
-            id='password'
-            value={pass}
-            autoComplete='current-password'
-          />
+            <form className={classes.form} noValidate>
+              <TextField
+                placeholder='user o admin'
+                onChange={setearUser}
+                variant='outlined'
+                margin='normal'
+                required
+                error={user === "" ? true : false}
+                fullWidth
+                id='email'
+                label='Usuario'
+                name='email'
+                autoComplete='email'
+                autoFocus
+                value={user}
+              />
+              <TextField
+                placeholder='123456'
+                onChange={setearPass}
+                variant='outlined'
+                margin='normal'
+                error={pass === "" ? true : false}
+                required
+                fullWidth
+                name='password'
+                label='Contraseña'
+                type='password'
+                id='password'
+                value={pass}
+                autoComplete='current-password'
+              />
 
-          <Button
-            disabled={pass === "" || user === "" ? true : false}
-            onClick={goToLogin}
-            fullWidth
-            variant='contained'
-            color='primary'
-            className={classes.submit}>
-            Ingresar
-            {loading && <CircularProgress color='secondary' size={30} />}
-          </Button>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
+              <Button
+                disabled={pass === "" || user === "" ? true : false}
+                onClick={goToLogin}
+                fullWidth
+                variant='contained'
+                color='primary'
+                className={classes.submit}>
+                Ingresar
+                {loading && <CircularProgress color='secondary' size={30} />}
+              </Button>
+            </form>
+          </div>
+          <Box mt={8}>
+            <Copyright />
+          </Box>
+        </Container>
+      </CardContent>
+    </Card>
   );
 }
